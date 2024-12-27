bdb7c9f3390bad879ef783b69ad8bcdc539924d6
payman-ygm <paymanmolaee2@gmail.com>
2024-12-27 15:31:23 +0000

    modify

--- admin.py
+++
@@ -1,8 +0,0 @@
-class Admin:
-    def add_user(self, data):
-        print(f"Adding user with data: {data}")
-        # Add logic for adding a user
-
-    def remove_user(self, data):
-        print(f"Removing user with data: {data}")
-        # Add logic for removing a user
--- patient.py
+++
@@ -1,8 +0,0 @@
-class Patient:
-    def register(self, data):
-        print(f"Registering patient with data: {data}")
-        # Add logic to register a patient
-
-    def update(self, data):
-        print(f"Updating patient record with data: {data}")
-        # Add logic to update patient details
